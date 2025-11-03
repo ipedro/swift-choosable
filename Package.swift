e715189faccc54ea0da009bb1f13a02da2f2841f
Pedro Almeida <ip4dro@gmail.com>
2025-11-03 23:00:45 +0100

    chore: exclude README/LICENSE from targets to silence SPM unhandled files warning

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 
 let package = Package(
     name: "swift-choosable",
+    // Exclude top-level non-source files so SwiftPM doesn't warn about unhandled files
+    exclude: ["README.md", "LICENSE"],
     products: [
         .library(
             name: "Choosable",
@@ -12,11 +14,13 @@ let package = Package(
         .target(
             name: "Choosable",
             path: ".",
+            exclude: ["README.md", "LICENSE"],
             sources: ["Choosable.swift"]),
         .testTarget(
             name: "ChoosableTests",
             dependencies: ["Choosable"],
             path: ".",
+            exclude: ["README.md", "LICENSE"],
             sources: ["ChoosableTests.swift"]),
     ]
 )
